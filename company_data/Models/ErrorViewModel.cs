125596da789977bc192a46e0d8e965ab139dcaac
baloma <gdetbaloma@gmail.com>
2023-05-25 16:13:38 +0800

    fist commit

---
+++ company_data/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace company_data.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
